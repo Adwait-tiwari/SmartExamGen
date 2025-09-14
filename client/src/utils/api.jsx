39d108423554d41740fa5ad32bb560c74902f3bd
Adwait Tiwari <tiwariadwait3@gmail.com>
2025-09-14 20:44:39 +0530

    Fixed Error

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_URL,
-    withCredentials: true, // if you are using cookies/sessions
+  baseURL: import.meta.env.VITE_API_URL, // ✅ Reads from Vite env
+  withCredentials: true,
 });
 
 export default api;
