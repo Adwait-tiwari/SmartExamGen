924a9d80a506889dea275aad0227a701742379a4
Adwait Tiwari <tiwariadwait3@gmail.com>
2025-09-24 20:30:16 +0530

    Fixed Netwrok Error Internal Server Error

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,10 @@
-import jwt from 'jsonwebtoken';
-
 export const generateToken = (user) => {
+    console.log("SESSION_SECRET:", process.env.SESSION_SECRET); // ✅ for debugging
     return jwt.sign({
         _id: user._id,
         username: user.username,
         email: user.email,
-        picture: user.picture || null, // Include picture if available
-        isGoogle: Boolean(user.googleId), // Set true if googleId exists
-    },
-        process.env.SESSION_SECRET, { expiresIn: '1h' }
-    );
+        picture: user.picture || null,
+        isGoogle: Boolean(user.googleId),
+    }, process.env.SESSION_SECRET, { expiresIn: '1h' });
 };
